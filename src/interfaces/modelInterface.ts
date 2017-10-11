3e2eb0c811c6cbac5801797f97b6280258826805
TinyBuilder <ivan.fernando.w@gmail.com>
2017-10-11 18:58:11 +0700

    Prettified some files.

--- src/interfaces/modelInterface.ts
+++ src/interfaces/modelInterface.ts
@@ -1,4 +1,5 @@
-import Store from '../interfaces/storeInterface'
+import Store from '../interfaces/storeInterface';
+
 export default abstract class Model {
   readonly data: Object;
   static store: Store;
