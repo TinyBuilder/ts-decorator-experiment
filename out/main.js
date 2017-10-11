b63f6c276b70d501077c38b8561e62b89f17920d
TinyBuilder <ivan.fernando.w@gmail.com>
2017-10-11 18:58:59 +0700

    Removed build artifacts.

--- out/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BasicModel_1 = require("./models/BasicModel");
-const basicModel = new BasicModel_1.default({ hello: 'world' });
-basicModel.update({ how: 'are you' }).save();
